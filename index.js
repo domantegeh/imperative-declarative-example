62c3dec2c06c3cb95d03dbd800b7423405f8387a
doman-tegeh <tegeh.add@gmail.com>
2023-12-19 22:56:52 +0800

    add examples of reusable function

--- index.js
+++ index.js
@@ -12,3 +12,61 @@ console.log(newNamesWithExcMark);
 const names1 = ["Harry", "Ron", "Jeff", "Thomas"];
 const newNamesWithExcMark1 = names.map((name) => `${name}!`);
 console.log(newNamesWithExcMark1);
+
+// reusable function
+const newArray = ["Harry", "Ron", "Jeff", "Thomas"];
+const newArray01 = newArray.map((name) => {
+  return `${name}!`;
+});
+console.log(newArray01);
+
+const truthyArray = [1, "", "Hallo", 0, null, "Harry", 14];
+const newTruthyArray = truthyArray.filter((item) => Boolean(item));
+console.log(newTruthyArray);
+
+const students = [
+  {
+    name: "Harry",
+    score: 60,
+  },
+  {
+    name: "James",
+    score: 88,
+  },
+  {
+    name: "Ron",
+    score: 90,
+  },
+  {
+    name: "Bethy",
+    score: 75,
+  },
+];
+const eligibleForScholarshipStudents = students.filter(
+  (student) => student.score > 85
+);
+console.log(eligibleForScholarshipStudents);
+
+const totalScore = students.reduce((acc, student) => acc + student.score, 0);
+console.log(totalScore);
+
+const oddScore = students.some((student) => student.score % 2 != 0);
+console.log(oddScore);
+
+const findJames = students.find((student) => student.name === "James");
+console.log(findJames);
+
+const compareScore = (a, b) => {
+  return a.score - b.score;
+};
+const sortScore = students.sort(compareScore);
+console.log(sortScore);
+
+const examPassed = students.every((student) => student.score >= 60);
+console.log(examPassed);
+
+let i = 1;
+const studentsData = students.forEach((student) => {
+  console.log(`${i} ${student.name}`);
+  i++;
+});
